a5fa3862f7c7d71b76f82e981fde6a920231b053
Amnesia <craksyw@gmail.com>
2022-03-23 15:15:52 +0100

    fix orderline add. add static `FromProduct` method

--- ErpSystemOpgave/ErpSystemOpgave/Data/SalesOrderLine.cs
+++ ErpSystemOpgave/ErpSystemOpgave/Data/SalesOrderLine.cs
@@ -4,7 +4,23 @@ namespace ErpSystemOpgave.Data;
 
 public class SalesOrderLine
 {
-    public int ProductId  { get; set; }
-    public double Amount { get; set; } // angivet i enhed svarende til produktet
-    public decimal TotalPrice => (decimal) 0.0; // TODO: calculate total price based on the product and amount
+    public static SalesOrderLine FromProduct(Product product, decimal amount)
+    {
+        return new SalesOrderLine(product.SalePrice, product.Name, product.Description, product.Unit, amount);
+    }
+    public SalesOrderLine(decimal price, string productName, string? productDescription, ProductUnit unit, decimal amount)
+    {
+        Price = price;
+        ProductName = productName;
+        ProductDescription = productDescription;
+        Unit = unit;
+        Amount = amount;
+    }
+
+    public decimal TotalPrice => Price *  Amount;
+    public decimal Price { get; set; }
+    public String ProductName { get; set; }
+    public String? ProductDescription { get; set; }
+    public ProductUnit Unit { get; set; }
+    public decimal Amount { get; set; } 
 }
