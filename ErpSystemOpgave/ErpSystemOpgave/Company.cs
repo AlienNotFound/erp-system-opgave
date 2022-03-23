fcb85508ff4e048675072ebf033fa6d06bcfb8bc
KanunaHaskai <mathiasbojelarsen03@gmail.com>
2022-03-23 13:26:25 +0100

    Added all the classes + DataBase class

---
+++ ErpSystemOpgave/ErpSystemOpgave/Company.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ErpSystemOpgave
+{
+    public class Company
+    {
+        public int City
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int CompanyNaame
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int Country
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int Number
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int Street
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int ZipCode
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}
---
+++ ErpSystemOpgave/ErpSystemOpgave/SalesOrder.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ErpSystemOpgave
+{
+    public class SalesOrder
+    {
+        public int Complete
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int Created
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int DeliveryAddress
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int ID
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int OrderNumber
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public Customer Customer
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int Lines
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public System.Collections.Generic.List<Orderline> Jens
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}
