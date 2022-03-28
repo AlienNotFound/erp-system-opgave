2e016d7ac1786760751b6723ec8e0e1019bb9a2b
Amnesia <craksyw@gmail.com>
2022-03-28 10:00:25 +0200

    add menu method. cleanup tracked build files

--- ErpSystemOpgave/ErpSystemOpgave/Program.cs
+++ ErpSystemOpgave/ErpSystemOpgave/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TECHCOOL.UI;
+
+namespace ErpSystemOpgave;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+
+    }
+    public static void ShowMenu(params (String Description, Action Action)[] items)
+    {
+        var menu = new ListPage<MenuItem>();
+        menu.AddColumn("Action:", "Description");
+        foreach (var (desc, action) in items)
+        {
+            menu.Add(new MenuItem(desc, action));
+        }
+        menu.Select().Action.Invoke();
+    }
+}
+record MenuItem(string Description, Action Action);
