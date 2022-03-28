381dd5ef434f7eb6374e1ae97a7cb67bf45f12d1
[Adam] <[adam7305@elev.techcollege.dk]>
2022-03-28 10:38:32 +0200

    U1: Connected to database

--- ErpSystemOpgave/ErpSystemOpgave/DataBase.cs
+++ ErpSystemOpgave/ErpSystemOpgave/DataBase.cs
@@ -1,12 +1,124 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using TECHCOOL.UI;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+//using ErpSystemOpgave.Data;
 
 namespace ErpSystemOpgave
 {
-    internal class DataBase
+    public class DataBase
     {
+        public void GetAllProducts()
+      {
+          string connectionString = @"Server=ADAMS-BÆRBAR\MSSQLSERVER01;Database=erp_system;Trusted_Connection=True;";
+          SqlConnection connection = new SqlConnection(connectionString);
+          connection.Open();
+
+          SqlDataReader dt;
+          SqlCommand cmd = new SqlCommand("SELECT * FROM tbl_productmodule", connection);
+          dt = cmd.ExecuteReader();
+          
+          ListPage<ProductDetails> listPage = new ListPage<ProductDetails>();
+          while (dt.Read())
+          {
+              listPage.Add(new ProductDetails(
+                  Convert.ToInt32(dt["id"]),
+                  dt["productname"].ToString(),
+                  dt["details"].ToString(),
+                  Convert.ToInt32(dt["stockunits"]),
+                  Convert.ToDecimal(dt["buyprice"]),
+                  Convert.ToDecimal(dt["salesprice"]),
+                  dt["location"].ToString(),
+                  Convert.ToDecimal(dt["salesprice"]),
+                  dt["unit"].ToString(),
+                  Convert.ToDouble(dt["avancepercent"]),
+                  Convert.ToDouble(dt["avancekroner"])));
+          }
+          
+          listPage.AddColumn("Varenr.", "ProductNumber");
+          listPage.AddColumn("Produktnavn", "Name");
+          listPage.AddColumn("Lagerantal", "StockUnits");
+          listPage.AddColumn("Købspris", "BuyPrice");
+          listPage.AddColumn("Salgspris", "SalesPrice");
+          listPage.AddColumn("Avance i procent", "AvancePercent");
+        
+          //Prints the selected product name out in the console, after pressing enter. Preparation for P3 
+          Console.WriteLine("Valgte: " + listPage.Select().Name);
+          connection.Close();
+      }
+        /*public List<SalesOrderHeader> salesOrderHeaders = new List<SalesOrderHeader>();
+        //salesOrderHeaders.Add(new SalesOrderHeader(3, 24,OrderState.Created,20, new List<SalesOrderLine>()));
+        public SalesOrderHeader GetSalesOrderById(int orderId)
+        {
+            var Order = salesOrderHeaders.Find(id => id.OrderNumber == orderId);
+            Console.WriteLine("Ordre nummer: " + Order.OrderNumber
+                                               + " Kundeid: " + Order.CustomerId
+                                               + " Status: " + Order.State
+                                               + " Pris: " + Order.Price
+                                               //+ " Ordrelinje: " + salesOrderHeaders[orderId].OrderLines
+                                               );
+            return Order;
+        }
+
+        public void GetAllSalesOrders()
+        {
+            List<SalesOrderHeader> salesOrderHeaders = new List<SalesOrderHeader>();
+            salesOrderHeaders.Add(new SalesOrderHeader(3, 24,OrderState.Created,22, new List<SalesOrderLine>()));
+            salesOrderHeaders.Add(new SalesOrderHeader(2, 35,OrderState.Created,20, new List<SalesOrderLine>()));
+            salesOrderHeaders.Add(new SalesOrderHeader(5, 89,OrderState.Created,30, new List<SalesOrderLine>()));
+
+            for (int i = 0; i < salesOrderHeaders.Count; i++)
+            {
+                Console.WriteLine("Ordre nummer: " + salesOrderHeaders[i].OrderNumber
+                                                   + " Kundeid: " + salesOrderHeaders[i].CustomerId
+                                                   + " Status: " + salesOrderHeaders[i].State
+                                                   + " Pris: " + salesOrderHeaders[i].Price
+                                                   + " Oprettet: " + salesOrderHeaders[i].CreationTime
+                );
+            }
+        }
+
+        public void CreateSalesOrder(int OrderNumber, int CustomerId, decimal Price)
+        {
+            List<SalesOrderHeader> salesOrderHeaders = new List<SalesOrderHeader>();
+            salesOrderHeaders.Add(new SalesOrderHeader(OrderNumber, CustomerId, OrderState.Created, Price, new List<SalesOrderLine>()));
+        }
+
+        public void UpdateSalesOrder(int OrderNumber, int CustomerId, decimal Price)
+        {
+            List<SalesOrderHeader> salesOrderHeaders = new List<SalesOrderHeader>();
+            salesOrderHeaders.Add(new SalesOrderHeader(3, 24,OrderState.Created,22, new List<SalesOrderLine>()));
+            salesOrderHeaders.Add(new SalesOrderHeader(2, 35,OrderState.Created,20, new List<SalesOrderLine>()));
+            salesOrderHeaders.Add(new SalesOrderHeader(5, 89,OrderState.Created,30, new List<SalesOrderLine>()));
+            
+            var result = from s in salesOrderHeaders
+                where s.OrderNumber == OrderNumber
+                select s;
+            foreach (var salesOrder in result)
+            {
+                Console.WriteLine("\nOriginalt: ");
+                Console.WriteLine("Kunde id: " + salesOrder.CustomerId);
+                Console.WriteLine("Pris: " + salesOrder.Price);
+                salesOrder.CustomerId = CustomerId;
+                salesOrder.Price = Price;
+                Console.WriteLine("\nOpdateret: ");
+                Console.WriteLine("Kunde id: " + salesOrder.CustomerId);
+                Console.WriteLine("Pris: " + salesOrder.Price);
+            }
+        }
+
+        public void DeleteSalesOrder(int OrderNumber)
+        {
+            List<SalesOrderHeader> salesOrderHeaders = new List<SalesOrderHeader>();
+            salesOrderHeaders.Add(new SalesOrderHeader(3, 24,OrderState.Created,22, new List<SalesOrderLine>()));
+            salesOrderHeaders.Add(new SalesOrderHeader(2, 35,OrderState.Created,20, new List<SalesOrderLine>()));
+            salesOrderHeaders.Add(new SalesOrderHeader(5, 89,OrderState.Created,30, new List<SalesOrderLine>()));
+
+            salesOrderHeaders.RemoveAll(s => s.OrderNumber == OrderNumber);
+        }*/
     }
 }
