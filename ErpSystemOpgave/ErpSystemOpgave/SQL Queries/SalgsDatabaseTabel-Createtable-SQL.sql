f37f58189a683436bc71929167d322fa308e9b95
[Adam] <[adam7305@elev.techcollege.dk]>
2022-04-29 13:25:44 +0200

    B6 & B7: Added tables for SalesOrderHeaders

---
+++ ErpSystemOpgave/ErpSystemOpgave/SQL Queries/SalgsDatabaseTabel-Createtable-SQL.sql
@@ -0,0 +1,29 @@
+USE H1PD021122_Gruppe3
+GO
+
+CREATE TABLE SalesOrderHeaders (
+		Id INT IDENTITY(1,1) NOT NULL,
+		CustomerId INT NOT NULL,
+		State VARCHAR(16) NOT NULL,
+		PriceSum DECIMAL NOT NULL,
+		Date DATE NOT NULL,
+		PRIMARY KEY(Id),
+		CONSTRAINT CustomerId FOREIGN KEY (CustomerId)
+		REFERENCES Customers (Id)
+)
+GO
+
+CREATE TABLE OrderLines (
+		Id INT IDENTITY(1,1) NOT NULL,
+		ProductId INT NOT NULL,
+		Quantity INT NOT NULL,
+		PricePer DECIMAL NOT NULL,
+		PriceSum DECIMAL NOT NULL,
+		SalesOrderHeaderId INT NOT NULL
+		PRIMARY KEY(Id)
+		CONSTRAINT ProductId FOREIGN KEY (ProductId)
+		REFERENCES Products (Id),
+		CONSTRAINT SalesOrderHeaderId FOREIGN KEY (SalesOrderHeaderId)
+		REFERENCES SalesOrderHeaders (Id)
+)
+GO
